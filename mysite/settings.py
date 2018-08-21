4898bd3b846d3245bdbe398c57ad96cfbcd44a80
admin <contact@mabel.li>
2018-08-21 10:06:26 +0100

    Modified templates to display posts from database. Post mod time added to model.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '52ua*jcgv#^9o%h)94tl@0*7+k$yix^(rtkbu(r$)l3n&=y$&n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
